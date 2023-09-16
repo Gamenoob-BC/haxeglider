ddfc6080fde3a58fd29c98cb7dda1aa0b7b77b3e
Gamenoob-BC <143728735+Gamenoob-BC@users.noreply.github.com>
2023-09-16 19:56:27 +0200

    Updates in NPM stuff

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
-const tf = require('@tensorflow/tfjs-node');
+const tf = require('@tensorflow/tfjs');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
